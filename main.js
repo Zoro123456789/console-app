cfde7a5bad5f8fb84d5f99850e19ba00c7d0aa90
Zoro123456789 <132527961+Zoro123456789@users.noreply.github.com>
2023-08-29 20:54:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var fname="Vrishali";
+console.log("first name:"+fname);
+
+var lname="joshi";
+console.log("last name:"+lname);
+
+var fullname=fname.concat(lname);
+console.log("full name:"+fullname);
+
+var length_fullname=fullname.length;
+console.log("length of fullname:"+length_fullname);
+
+var new_name=fullname.replace("joshi","visual studio code");
+console.log("newname=:"+new_name);
+
+var game="FOOTBALL";
+console.log("game:"+game);
+
+var game_lower=game.toLowerCase();
+console.log("gamelower:"+game_lower);
+
+var num1=100;
+
+var num2=20
+
+var total=num1+num2;
+
+var sub=num1-num2;
+
+var div=num1/num2;
+
+var product=num1*num2;
+
+console.log("num1:"+num1);
+
+console.log("num2:"+num2);
+
+console.log("total:"+total);
+
+console.log("sub"+sub);
+
+console.log("division"+div);
+
+console.log("product:"+product);
